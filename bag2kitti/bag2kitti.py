0c19a5dccf3d3b846497cf1f8761e725899fe846
pushyamikaveti <kaveti.p@northeastern.edu>
2021-10-14 12:03:46 -0400

    created a new package to convert the ros bags into kitti data format. Currently ony images are being written. We might have to add code for velodyne and calibration params in future.

--- bag2kitti/bag2kitti.py
+++ bag2kitti/bag2kitti.py
@@ -10,7 +10,9 @@ from cv_bridge import CvBridge, CvBridgeError
 
 '''
 @author : Pushyami Kaveti
-Simple script to take a rosbag and write images topics as images
+Simple script to take a rosbag and write image topics to disk in kitti dataset format.
+It uses a config file to specify various options for selection like topics, 
+time range, frequency etc.
 '''
 import rospy
 import cv2
@@ -24,17 +26,8 @@ import os
 import argparse
 import yaml
 
-'''
-@author: Pushyami Kaveti 
-This is a tool to select only required images from a rosbag. 
-The script goes through each image in the bag one by one and 
-shows in a window, it has key bindings to  select the image 
-which will be written to another bag file. It uses a config 
-file to specify various options for selection like topics, 
-time range, frequency etc.
-'''
 
-class Bag2Image:
+class Bag2Kitti:
     def __init__(self,read_bag_path, write_folder_path, write_bag_path, config):
         self.viz=False
         self.calib=False
@@ -42,12 +35,14 @@ class Bag2Image:
         self.write_folder_path = write_folder_path
         self.bridge = CvBridge()
         self.topics =  self.image_pub_topics  = config['topics']
-
+        self.num_frames = config['num_frames']
+        self.bag_name = os.path.basename(read_bag_path)[:-4]
         self.num_topics = len(self.topics)
-        for i in range(self.num_topics):
-            pathe = os.path.join(self.write_folder_path,"cam"+str(i))
-            if not os.path.exists(pathe):
-                os.makedirs(pathe)
+        #for i in range(self.num_topics):
+        #    pathe = os.path.join(self.write_folder_path,"image_"+str(i),"data")
+        #    if not os.path.exists(pathe):
+        #        os.makedirs(pathe)
+        #self.timestamp_file = open(os.path.join(self.write_folder_path,'timestamps.txt'), 'a')
         rospy.init_node('Bag2Image', anonymous = True)
         self.read_bag = rosbag.Bag(read_bag_path)
         self.start = rospy.Time(config['start_time'] + self.read_bag.get_start_time())
@@ -61,6 +56,7 @@ class Bag2Image:
         topiclist=[]
         msg_list=[]
         count = 0
+        part_num=0
         print(type(self.read_bag.get_start_time()))
         for topic, msg, t in self.read_bag.read_messages(topics=self.topics , start_time= self.start , end_time= self.end):
             if oldt == msg.header.stamp:
@@ -69,6 +65,17 @@ class Bag2Image:
                 #print(msg.header.frame_id)
             else:
                 oldt =  msg.header.stamp
+                if count % self.num_frames == 0:
+                    if count != 0: 
+                        self.timestamp_file.close()
+                    count = 0
+                    part_num = part_num + 1
+                    for i in range(self.num_topics):
+                        pathe = os.path.join(self.write_folder_path,self.bag_name+"_"+str(part_num).zfill(4),"image_"+str(i),"data")
+                        if not os.path.exists(pathe):
+                            os.makedirs(pathe)
+                    self.timestamp_file = open(os.path.join(self.write_folder_path,self.bag_name+"_"+str(part_num).zfill(4),'timestamps.txt'), 'a')  
+
                 if len(topiclist) == self.num_topics and topiclist==self.topics :
                     if count % self.frequency == 0:
                         #print("Writing to bag:")
@@ -80,8 +87,9 @@ class Bag2Image:
                             cv_img_list.append(cv_image)
 
                         for i in range(self.num_topics):
-                            print(os.path.join(self.write_folder_path,"cam"+str(i),str(oldt.secs)+"."+str(oldt.nsecs)+".jpg"))
-                            cv2.imwrite(os.path.join(self.write_folder_path,"cam"+str(i),str(oldt.secs)+"."+str(oldt.nsecs)+".jpg"), cv_img_list[i])
+                            print(os.path.join(self.write_folder_path,self.bag_name+"_"+str(part_num).zfill(4),"image_"+str(i),"data",str(count).zfill(10)+".jpg"))
+                            cv2.imwrite(os.path.join(self.write_folder_path,self.bag_name+"_"+str(part_num).zfill(4),"image_"+str(i),"data",str(count).zfill(10)+".jpg"), cv_img_list[i])
+                        self.timestamp_file.write(str(oldt.secs)+"."+str(oldt.nsecs)+"\n")
                     count = count + 1
                 print("starting list\n")
 
@@ -90,6 +98,7 @@ class Bag2Image:
                 topiclist.append(topic)
                 msg_list.append(msg)
         self.write_bag.close()
+        self.timestamp_file.close()
 
 
 if __name__=="__main__":
@@ -107,6 +116,4 @@ if __name__=="__main__":
     with open(args.config_file, 'r') as f:
         config = yaml.safe_load(f)
     print(config)
-    bs = Bag2Image(args.input_bagfile , args.output_dir , args.output_filename, config)
-    #bs = BagSelector("2019_Aug6_indoor_rig_calib_selected.bag" , "/home/auv/testing_bagselector", "")
-    #/media/auv/Untitled/2019-08-13-00-09-48.bag
+    bs = Bag2Kitti(args.input_bagfile , args.output_dir , args.output_filename, config)
